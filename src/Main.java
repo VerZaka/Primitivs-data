e835fc39ea78437cba1068dd7edb473c6503a7aa
Verona <valetta7777@list.ru>
2023-05-21 00:40:40 +0300

    chenges Gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 17765; // стоимости билета
+        int bonuse = (amount / 20); // количество рублей для одной бонусной мили
+        System.out.println(bonuse + "бонусов");
+
+
+    }
+}
